47589e608f010732d039b88f1384b9c7b87895b9
Hannathevik-boop <hannathevik.com@gmail.com>
2026-06-19 07:49:47 +0200

    logo

--- src/app/components/PatternSection.tsx
+++ src/app/components/PatternSection.tsx
@@ -4,7 +4,7 @@ import { SectionLabel } from "./BrandSection";
 import { MBHMark } from "./MBHLogo";
 import { IconHydraulic, IconPropeller, IconBoat, IconPump, IconCylinder } from "./MBHIcons";
 import lynetSvg from "./ui/svg-logos-test/lynet_.svg";
-import mbLightSvg from "./ui/svg-logos-test/mb-light.svg";
+import mbLightSvg from "./ui/svg-logos-test/mbh-blaa.svg";
 
 type PatternKey = "hex" | "wave" | "watermark" | "grid" | "hatch" | "scatter";
 
